fixed precision bug fix

hw2.py
@@ -8,8 +8,6 @@ from numpy.linalg import norm
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
 
-import math
-
 ### File IO and processing
 
 class Document(NamedTuple):
@@ -247,31 +245,38 @@ def mean_precision2(results, relevant):
     precision = 0
     for i in range(1, 11):
         precision += precision_at(i / 10, results, relevant)
-    return precision  / 10
+    return precision / 10
 
 def norm_recall(results, relevant):
+    N = len(results)
+    Rel = len(relevant)
+
     sum_of_ranks = 0
     for doc in relevant:
         sum_of_ranks += results[doc] + 1 # add 1 since ranks are counted from 1 not 0 and are thus offset by -1 
+    sum_i = 0
     for i in range(1, Rel):
-        i += i
+        sum_i += i
 
+    return 1 - ((sum_of_ranks - sum_i)/(Rel*(N-Rel)))
+
+def factorial(num):
+    if num == 0:
+        return 1
+    return (num * factorial(num - 1))
+
+def norm_precision(results, relevant):
     N = len(results)
     Rel = len(relevant)
 
-    return 1 - (sum_of_ranks - i)/(Rel*(N-Rel))
-
-def norm_precision(results, relevant):
     sum_of_ranks = 0
+    sum_i = 0
     for doc in relevant:
         sum_of_ranks += np.log2(results[doc] + 1) # add 1 since ranks are counted from 1 not 0 and are thus offset by -1 
     for i in range(1, Rel):
-        i += np.log2(i)
+        sum_i += np.log2(i)
 
-    N = len(results)
-    Rel = len(relevant)
-
-    return 1 - (sum_of_ranks - i)  / (np.log(math.factorial(N)) / (math.factorial(N - Rel)) * math.factorial(Rel))
+    return 1 - ((sum_of_ranks - sum_i)  / (N * np.log2(N) - (N - Rel) * np.log2(N - Rel) - Rel * np.log2(Rel))) # approximate
 
 
 ### Extensions